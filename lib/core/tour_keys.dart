49c6e70087e657cdc3be92499d09905ac7b9f85f
rdmbtc <ntraid03@gmail.com>
2026-07-10 08:31:57 +0500

    fix: add missing tour keys to fix Vercel build

--- lib/core/tour_keys.dart
+++ lib/core/tour_keys.dart
@@ -7,3 +7,6 @@ final GlobalKey tourPortfolioKey = GlobalKey(debugLabel: 'tourPortfolioKey');
 final GlobalKey tourCreatorsKey = GlobalKey(debugLabel: 'tourCreatorsKey');
 final GlobalKey tourAgentKey = GlobalKey(debugLabel: 'tourAgentKey');
 final GlobalKey tourProfileKey = GlobalKey(debugLabel: 'tourProfileKey');
+final GlobalKey tourMarketKey = GlobalKey(debugLabel: 'tourMarketKey');
+final GlobalKey tourSwarmKey = GlobalKey(debugLabel: 'tourSwarmKey');
+final GlobalKey tourUsdcKey = GlobalKey(debugLabel: 'tourUsdcKey');
